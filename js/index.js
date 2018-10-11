7325e652ae0ed5c76e659c6ffd4b09d2e56989f6
Wei <wuwj2001@gmail.com>
2018-10-10 17:18:18 -0700

    generic function created

--- js/index.js
+++ js/index.js
@@ -4,3 +4,33 @@ import test from './script2.js';
 console.log('test');
 
 test();
+
+// Create your reference to your data. In this case we are rerencing the entire database at '/'
+var databaseRef = firebase.database().ref('/');
+
+//Listen to your database and get a snapshot at that reference
+databaseRef.once('value').then(function(snapshot) {
+    //Get the values from the snapshot of the data
+    const databaseValues = snapshot.val();
+    //Log all the data to the console
+    console.log(databaseValues.memos[0]);
+
+    const memos = databaseValues.memos
+
+    console.log(typeof(memos[0].author))
+
+    appendNote(memos[0]);
+
+    function appendNote(note) {
+        console.log(note.author);
+        $("#memo-board").append(
+           "<div class='card-body'>" + 
+           "<h5 class='card-title'>" + note.author + "</h5>" +
+           "<p class='card-text'>" + note.quote + "</p>" +
+           "<p>" + note.date + "</p>" +
+           "</div>"
+        );
+        
+    }
+});
+
